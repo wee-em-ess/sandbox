16098d70c29e586b1f36cc8a209d75de0a5a916f
wee-em-ess <136472062+wee-em-ess@users.noreply.github.com>
2025-04-19 12:50:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ canvas.height = window.innerHeight;
 
 const container = document.getElementById("container");
 const toolbar = document.getElementById("toolbar");
+const info = document.querySelectorAll('#info')[0]
+const touches = document.querySelectorAll('#touches')[0]
 
 container.addEventListener("click", containerClickHandler);
 
@@ -17,19 +19,25 @@ const canvasFlags = {
     isNewStrokes: false,
     isBgImg: false,
 	isGuides: false,
+    isPressure:false,
+    isTouch: false,
+    isThrottle: false,
+    isRejectPalm: false,
 };
 
 // Canvas Properties Object
 const canvasProps = {
     lineCap: "round",
     lineJoin: "bevel",
-    tipWidth: 1 * 1,
+    tipWidth: 0.5,
 	tipPush: 0,
     strokeOpacity: 1,
 	strokeScale: 1,
     globalOpacity: 1,
-    strokeColor: "#dc143c", // Crimson
+    strokeColor: "#000000", // Crimson
 	fillColor: "#ffffff",
+    smoothingFactor: 0.5,
+    palmRadius: 50,
     blendingMode: "source-over",
 	toolSize: "xsStroke",
 	toolType: "marker",
@@ -38,6 +46,9 @@ const canvasProps = {
 };
 
 let points = [];
+let pType = "pV1";
+let gType;
+
 //------------------------------------------------------------------------------------//
 
 // Define the named event handler function
@@ -45,7 +56,7 @@ function containerClickHandler(event) {
 const target = event.target;
 
 
-   if (target && target.classList.contains("button")) {
+    if (target && target.classList.contains("button")) {
     
         //target.classList.toggle("button-selected");
 
@@ -62,6 +73,85 @@ const target = event.target;
         clearCanvas(context, canvas);
 
     }
+
+    if (target && target.id === "pressure") {
+    
+        target.classList.toggle("button-selected");
+        if (canvasFlags.isPressure) {canvasFlags.isPressure = false}
+        else {canvasFlags.isPressure = true}
+
+    }
+
+    if (target && target.name === "pType") {
+        
+        pType = target.value;
+        console.log (target.value);
+
+    }
+
+    if (target && target.id === "pSlider") {
+        
+        canvasProps.smoothingFactor = target.value/100;
+        console.log (target.value/100);
+
+    }
+
+    if (target && target.id === "palmSlider") {
+        
+        canvasProps.palmRadius = target.value;
+        console.log (target.value);
+
+    }
+
+
+    if (target && target.id === "guides") {
+    
+        target.classList.toggle("button-selected");
+        if (canvasFlags.isGuides) {canvasFlags.isGuides = false}
+        else {canvasFlags.isGuides = true}
+
+    }
+
+    if (target && target.name === "gType") {
+        
+        gType = target.value;
+        console.log (target.value);
+
+    }
+
+    if (target && target.id === "touch") {
+    
+        target.classList.toggle("button-selected");
+        if (canvasFlags.isTouch) {canvasFlags.isTouch = false}
+        else {canvasFlags.isTouch = true}
+
+    }
+
+    if (target && target.id === "reject-palm") {
+    
+        target.classList.toggle("button-selected");
+        if (canvasFlags.isRejectPalm) {canvasFlags.isRejectPalm = false}
+        else {canvasFlags.isRejectPalm = true}
+
+    }
+
+    if (target && target.id === "throttle-stroke") {
+    
+        target.classList.toggle("button-selected");
+        if (canvasFlags.isThrottle) {canvasFlags.isThrottle = false}
+        else {canvasFlags.isThrottle = true}
+
+    }
+
+    if (target && target.id === "pV1") {
+    
+        target.classList.toggle("button-selected");
+        if (canvasFlags.isThrottle) {canvasFlags.isThrottle = false}
+        else {canvasFlags.isThrottle = true}
+
+    }
+
+   
 }
 
 //------------------------------------------------------------------------------------//
@@ -121,34 +211,55 @@ function clearCanvas (context, canvas) {
 
 let c;
 let cnvScale = 1;
+let device;
+
 
 function handleStartDrawing(e) {
 
-		if (e.touches && e.touches.length >= 2) return;
-		e.preventDefault();
+    let pressure = 1;
+    let x, y;
+    let validTouches;
 
-		let pressure = 1;
-		let x, y;
+    if (e.touches) {           
+
+        if (!canvasFlags.isPalmTouch) {
+            
+            const validTouches = Array.from(e.touches)}
+        
+        else {
+            
+            const validTouches = Array.from(e.touches).filter(touch => !isPalmTouch(touch));
+            
+        }
+        
+        
+        if (validTouches.length === 0 || validTouches.length >= 2 ) { return }
+
+    }
+
+    e.preventDefault();
+    
+    const cnv_X = canvas.getBoundingClientRect().left
+    const cnv_Y = canvas.getBoundingClientRect().top
 
 
-		const cnv_X = canvas.getBoundingClientRect().left;
-		const cnv_Y = canvas.getBoundingClientRect().top;
-		
 
-        if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
+    if (validTouches && validTouches[0] && typeof validTouches[0]["force"] !== "undefined") {
 
-			if (e.touches[0]["force"] > 0) {
+        if (canvasFlags.isPressure && validTouches[0]["force"] > 0) {
+            
+            pressure = validTouches[0]["force"];
+            device = "Force Touch / Apple Pencil"
+        }
+
+        else {device = "Touch"}
+        
+        x = (validTouches[0].clientX - cnv_X) * cnvScale;
+        y = (validTouches[0].clientY - cnv_Y) * cnvScale;
+                    
+    } else {
 				
-				pressure = e.touches[0]["force"];
-			
-			}
-			
-			x = (e.touches[0].clientX - cnv_X) * cnvScale;
-			y = (e.touches[0].clientY - cnv_Y) * cnvScale;
-
-		} else {
-				
-			pressure = 1.0;
+			pressure = 1;
 			x = e.offsetX * cnvScale;
 			y = e.offsetY * cnvScale;
 			
@@ -157,8 +268,14 @@ function handleStartDrawing(e) {
     
 	canvasFlags.isDrawing = true;
 
-	canvasProps.tipPush = Math.log(pressure + 1) * 40;
+    if (pType === "pV1") {canvasProps.tipPush = pressure;}    
+	else if (pType === "pV2") {canvasProps.tipPush = Math.log(pressure + 1) * 40;}
+    else if (pType === "pV3") {canvasProps.tipPush = canvasProps.smoothingFactor * canvasProps.tipWidth + (1 - canvasProps.smoothingFactor) * (pressure * canvasProps.tipWidth);}
+    else if (pType === "pV4") {canvasProps.tipPush = Math.pow(pressure, 2) * canvasProps.tipWidth;}
+    else if (pType === "pV5") {canvasProps.tipPush = (Math.exp(pressure) - 1) / (Math.exp(1) - 1) * canvasProps.tipWidth;}
+
 	const w = (Math.round(canvasProps.tipPush * canvasProps.tipWidth * canvasProps.strokeScale * 10) / 10);
+
 	x = (Math.round(x * 10) / 10);
 	y = (Math.round(y * 10) / 10);
 	c = `rgba(${parseInt(canvasProps.strokeColor.slice(1,3),16)},${parseInt(canvasProps.strokeColor.slice(3, 5),16)},${parseInt(canvasProps.strokeColor.slice(5,7),16)},${canvasProps.strokeOpacity})`;
@@ -171,6 +288,18 @@ function handleStartDrawing(e) {
 	/// calling Draw Function ///
 	canvasDraw(points, context, 1);
 
+    info.innerHTML = `
+        device = ${device} <br/>
+        pressure = ${pressure} <br/>
+        calculatedForce = ${canvasProps.tipPush} <br/>
+        touchType = ${touch.touchType} ${touch.touchType === 'direct' ? 'Touch' : 'Device'} <br/>
+        radiusX = ${touch.radiusX} <br/>
+        radiusY = ${touch.radiusY} <br/>
+        rotationAngle = ${touch.rotationAngle} <br/>
+        altitudeAngle = ${touch.altitudeAngle} <br/>
+        azimuthAngle = ${touch.azimuthAngle} <br/>
+      `
+
 }
 
 //------------------------------------------------------------------------------------//
@@ -180,50 +309,87 @@ function handleStartDrawing(e) {
 function handleKeepDrawing(e) {
 
 		if (!canvasFlags.isDrawing) return;
-		if (e.touches && e.touches.length >= 2) return;
+
+        let pressure = 1;
+		let x, y;
+        let validTouches;
+
+		if (e.touches) {           
+
+            if (!canvasFlags.isPalmTouch) {
+                
+                const validTouches = Array.from(e.touches)}
+            
+            else {
+                
+                const validTouches = Array.from(e.touches).filter(touch => !isPalmTouch(touch));
+                
+            }
+            
+            
+            if (validTouches.length === 0 || validTouches.length >= 2 ) { return }
+
+        }
+
 		e.preventDefault();
 		
-		let pressure = 1;
-		let x, y;
-
-		
-
 		const cnv_X = canvas.getBoundingClientRect().left
 		const cnv_Y = canvas.getBoundingClientRect().top
 
-		if (e.touches && e.touches[0] && typeof e.touches[0]["force"] !== "undefined") {
 
-			if (e.touches[0]["force"] > 0) {
+
+		if (validTouches && validTouches[0] && typeof validTouches[0]["force"] !== "undefined") {
+
+			if (canvasFlags.isPressure && validTouches[0]["force"] > 0) {
 				
-				pressure = e.touches[0]["force"];
-
+				pressure = validTouches[0]["force"];
+                device = "Force Touch / Apple Pencil"
 			}
+
+            else {device = "Touch"}
 			
-			x = (e.touches[0].clientX - cnv_X) * cnvScale;
-			y = (e.touches[0].clientY - cnv_Y) * cnvScale;
+			x = (validTouches[0].clientX - cnv_X) * cnvScale;
+			y = (validTouches[0].clientY - cnv_Y) * cnvScale;
 						
 		} else {
 			
 			pressure = 1.0;
 			x = e.offsetX * cnvScale;
 			y = e.offsetY * cnvScale;
-		
+            device = "Mouse"
 		}
 
         
-		
-		// lineWidth = Math.log(pressure + 1) * 40 * 0.2 + lineWidth * 0.8;
-		canvasProps.tipPush = Math.log(pressure + 1) * 40 * 0.2 + canvasProps.tipPush * 0.8;
+		if (pType === "pV1") {canvasProps.tipPush = pressure;}
+		else if (pType === "pV2") {canvasProps.tipPush = Math.log(pressure + 1) * 40 * 0.2 + canvasProps.tipPush * 0.8; }
+        else if (pType === "pV3") {canvasProps.tipPush = canvasProps.smoothingFactor * canvasProps.tipWidth + (1 - canvasProps.smoothingFactor) * (pressure * canvasProps.tipWidth);}
+        else if (pType === "pV4") {canvasProps.tipPush = Math.pow(pressure, 2) * canvasProps.tipWidth;}
+        else if (pType === "pV5") {canvasProps.tipPush = (Math.exp(pressure) - 1) / (Math.exp(1) - 1) * canvasProps.tipWidth;}
+
+
 		const w = (Math.round(canvasProps.tipPush * canvasProps.tipWidth * canvasProps.strokeScale * 10) / 10);
 		x = (Math.round(x * 10) / 10);
 		y = (Math.round(y * 10) / 10);
 		// const c = `rgba(${parseInt(canvasProps.strokeColor.slice(1,3),16)},${parseInt(canvasProps.strokeColor.slice(3,5),16)},${parseInt(canvasProps.strokeColor.slice(5,7),16)},${canvasProps.strokeOpacity})`;
-        console.log(pressure,w);
+
 		// Push data into points object
 		points.push({x,y,w,c});
 		
 		/// calling Draw Function ///
 		canvasDraw(points, context, 1);
+
+   
+        info.innerHTML = `
+        device = ${device} <br/>
+        pressure = ${pressure} <br/>
+        calculatedForce = ${canvasProps.tipPush} <br/>
+        touchType = ${touch.touchType} ${touch.touchType === 'direct' ? 'Touch' : 'Device'} <br/>
+        radiusX = ${touch.radiusX} <br/>
+        radiusY = ${touch.radiusY} <br/>
+        rotationAngle = ${touch.rotationAngle} <br/>
+        altitudeAngle = ${touch.altitudeAngle} <br/>
+        azimuthAngle = ${touch.azimuthAngle} <br/>
+      `
 
 }
 
@@ -278,14 +444,46 @@ function handlePauseDrawing(e) {
 
 //------------------------------------------------------------------------------------//
 
-// Handle Touch Events - Palm Rejection + Apple Pencil
 
-// Helper function for palm rejection
+
+// Helper function for palm rejection - // Handle Touch Events - Palm Rejection + Apple Pencil
 function isPalmTouch(touch) {
 
     // Example heuristic: Reject touches with large radii
-    return touch.radiusX > 50 || touch.radiusY > 50;
+    return touch.radiusX > canvasProps.palmRadius || touch.radiusY > canvasProps.palmRadius;
+
 }
+
+
+//------------------------------------------------------------------------------------//
+
+
+// // Helper Function to throttle mousemove and touchmove
+
+function throttle(func, freq = 60) {
+    const limit = 1000 / freq; // Calculate interval based on framerate
+    let lastFunc;
+    let lastRan;
+
+    return function(...args) {
+        const context = this;
+
+        if (!lastRan) {
+            func.apply(context, args);
+            lastRan = Date.now();
+        } else {
+            clearTimeout(lastFunc);
+            lastFunc = setTimeout(function() {
+                if ((Date.now() - lastRan) >= limit) {
+                    func.apply(context, args);
+                    lastRan = Date.now();
+                }
+            }, limit - (Date.now() - lastRan));
+        }
+    };
+}
+
+//------------------------------------------------------------------------------------//
 
 // Touch Start Handler
 function handleTouchStart(e){
